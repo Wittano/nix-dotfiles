bfe9aab46c0279878a45fad97e0ebe6552167050
Wittano <radoslaw.ratyna@gmail.com>
2022-03-20 14:01:59 +0100

    fix: add devices path for virt configuration

--- hosts/virt/hardware.nix
+++ hosts/virt/hardware.nix
@@ -2,7 +2,11 @@
 
   fileSystems = {
     "/" = {
-      device = "<DISK>";
+      device = "/dev/disk/by-label/ROOT";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-label/BOOT";
       fsType = "ext4";
     };
   };
