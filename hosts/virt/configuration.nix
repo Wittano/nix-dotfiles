4c633984a3c43577642ed02706e860a947219633
Wittano <radoslaw.ratyna@gmail.com>
2022-03-20 15:59:28 +0100

    feat: update virt configuration

--- hosts/virt/configuration.nix
+++ hosts/virt/configuration.nix
@@ -2,17 +2,7 @@
 
   imports = [ ./hardware.nix ./networking.nix ];
 
-  home-manager.users.wittano = ./../../home/pc;
-
   modules = {
-    desktop = {
-      openbox.enable = true;
-      apps = {
-        rofi.enable = true;
-        zeal.enable = true;
-        alacritty.enable = true;
-      };
-    };
     services = {
       ssh.enable = true;
       redshift.enable = true;
