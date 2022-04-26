c5805604eadcf0cc20133995b163270a0a6e56fe
Wittano <radoslaw.ratyna@gmail.com>
2022-04-26 10:07:51 +0200

    feat(virutalization): add ansible package

--- modules/hardware/virutalization.nix
+++ modules/hardware/virutalization.nix
@@ -26,7 +26,7 @@ in {
     users.users.wittano.extraGroups =
       [ (mkIf cfg.enableDocker "docker") "libvirtd" "vboxusers" ];
 
-    environment.systemPackages = with pkgs; [ virt-manager vagrant ];
+    environment.systemPackages = with pkgs; [ virt-manager vagrant ansible ];
 
     boot.kernelModules = [ "kvm-intel" ];
   };
