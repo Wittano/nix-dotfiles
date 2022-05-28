a508e8651a3f3dbfe7783ede25d32c486435d785
Wittano <radoslaw.ratyna@gmail.com>
2022-05-28 15:52:15 +0200

    chore(networking): change ip

--- hosts/pc/networking.nix
+++ hosts/pc/networking.nix
@@ -5,7 +5,7 @@
     interfaces.eno1 = {
       useDHCP = false;
       ipv4.addresses = [{
-        address = "192.168.1.165";
+        address = "192.168.1.5";
         prefixLength = 24;
       }];
     };
