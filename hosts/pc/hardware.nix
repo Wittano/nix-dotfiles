8991bced7937eed9587ccadeaad621cd9f449ef1
Wittano <radoslaw.ratyna@gmail.com>
2022-07-01 16:59:00 +0200

    fix(filesystem): changed filesystem mounts in PC profile

--- hosts/pc/hardware.nix
+++ hosts/pc/hardware.nix
@@ -4,13 +4,13 @@
       device = "/dev/disk/by-label/ROOT";
       fsType = "ext4";
     };
-    "/home" = {
-      device = "/dev/disk/by-label/HOME";
-      fsType = "xfs";
-    };
     "/boot/efi" = {
       device = "/dev/disk/by-label/BOOT";
       fsType = "vfat";
+    };
+    "/mnt/windows" = {
+      device = "/dev/disk/by-label/WIN";
+      fsType = "ext4";
     };
     "/mnt/backup" = {
       device = "/dev/disk/by-label/BACKUP";
