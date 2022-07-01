7b5c35717327e2d8216a295958e57ecc2ef1ae22
Wittano <radoslaw.ratyna@gmail.com>
2022-07-01 21:03:51 +0200

    feat(filesystem): removed windows dir

--- hosts/pc/hardware.nix
+++ hosts/pc/hardware.nix
@@ -8,10 +8,6 @@
       device = "/dev/disk/by-label/BOOT";
       fsType = "vfat";
     };
-    "/mnt/windows" = {
-      device = "/dev/disk/by-label/WIN";
-      fsType = "ext4";
-    };
     "/mnt/backup" = {
       device = "/dev/disk/by-label/BACKUP";
       fsType = "ext4";
