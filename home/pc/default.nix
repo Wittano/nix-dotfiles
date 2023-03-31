c169261413fc564b4e407c4b7149a11da0ff58a8
Wittano <radoslaw.ratyna@gmail.com>
2023-03-31 21:55:28 +0200

    feat(home): removed apps, which installed via flatpak

--- home/pc/default.nix
+++ home/pc/default.nix
@@ -2,26 +2,18 @@
 with lib.my;
 let
   programs = with pkgs; [
-    # Communicators
-    signal-desktop
-    spotify
-
     # Utils
     flameshot
-
-    # Organizations
-    obsidian
 
     # Web browser
     vivaldi
 
     # Apps
     thunderbird
-    gnome.nautilus
     gnome.file-roller
     keepassxc
     gnome.eog
-    krita
+    spotify
     evince
 
     # Dev
