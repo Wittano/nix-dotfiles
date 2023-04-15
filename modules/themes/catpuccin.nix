cad9e670546d48e47600bfc32fe24afd5972a160
Wittano <radoslaw.ratyna@gmail.com>
2023-04-15 12:22:46 +0200

    feat(theme): added new theme: catpuccin

--- modules/themes/catpuccin.nix
+++ modules/themes/catpuccin.nix
@@ -1,1 +1,24 @@
 # TODO Add catpuccin theme
+{ ... }: {
+  options = {
+    modules.themes.catppuccin = {
+      enable = mkEnableOption ''
+        Enable catppuccin theme as system theme
+      '';
+    };
+  };
+
+  config = mkIf (cfg.enable) {
+    home-manager.users.wittano.gtk = {
+      enable = true;
+      theme = {
+        name = "catppuccin";
+        package = pkgs.catppuccin-gtk;
+      };
+      iconTheme = {
+        name = "catppuccin";
+        package = pkgs.catppuccin-gtk;
+      };
+    };
+  };
+}
