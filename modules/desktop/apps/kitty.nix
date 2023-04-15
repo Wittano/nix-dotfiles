fb4a4d6c44a6ec15b4d6ada579b8ca25004af059
Wittano <radoslaw.ratyna@gmail.com>
2023-04-15 05:34:19 +0200

    chore: removed TODO

--- modules/desktop/apps/kitty.nix
+++ modules/desktop/apps/kitty.nix
@@ -12,7 +12,6 @@ with lib.my;
           };
       };
 
-      # TODO Add kitty themes https://github.com/dexpota/kitty-themes
       xdg.configFile = mkIf (cfg.enableDevMode == false) {
         "kitty".source = dotfiles.".config".kitty.source;
       };
