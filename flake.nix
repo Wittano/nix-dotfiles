214cd15b859e4fb56b050bb01fcd68f70a2ea754
Wittano <radoslaw.ratyna@gmail.com>
2023-07-29 22:09:44 +0200

    feat(hyprland): added basic configuration

--- flake.nix
+++ flake.nix
@@ -58,10 +58,12 @@
               "https://wittano.cachix.org"
               "https://nix-community.cachix.org"
               "https://cache.nixos.org/"
+              "https://hyprland.cachix.org"
             ];
             trusted-public-keys = [
               "wittano.cachix.org-1:fCQ8OZR/eyQc5ic1Ra6PIDQc0cox7bjS+S89DVnojgA="
               "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+              "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
             ];
           };
         };
---
+++ modules/desktop/hyprland.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, lib, dotfiles, unstable, ... }:
+with lib;
+with lib.my;
+let
+  cfg = config.modules.desktop.hyprland;
+in
+{
+  options.modules.desktop.hyprland = {
+    enable = mkEnableOption "Enable Hyperland(Wayland) desktop";
+    enableDevMode = mkEnableOption ''
+      Enable dev mode.
+      Special mode, that every external configuration will be mutable
+    '';
+  };
+
+  config = mkIf cfg.enable {
+    programs.hyprland = rec {
+      enable = true;
+      xwayland.enable = enable;
+      nvidiaPatches = config.modules.hardware.nvidia.enable;
+    };
+  };
+}
