2d0b343d329a30abe3c43a41789886d89cabf052
Wittano <radoslaw.ratyna@gmail.com>
2023-08-22 19:23:00 +0200

    feat(jvm): add gradle as service and home package

--- modules/dev/jvm.nix
+++ modules/dev/jvm.nix
@@ -1,9 +1,10 @@
-{ config, pkgs, lib, home-manager, ... }:
+{ config, pkgs, lib, home-manager, username, ... }:
 let
   inherit (lib) mkEnableOption mkIf;
 
   cfg = config.modules.dev.jvm;
-in {
+in
+{
   options = {
     modules.dev.jvm = {
       enable = mkEnableOption ''
@@ -13,11 +14,22 @@ in {
   };
 
   config = mkIf cfg.enable {
-    home-manager.users.wittano.home = {
-      packages = with pkgs; [ jetbrains.idea-ultimate jdk ];
-      file.".ideavimrc".text = ''
-        set rnu nu
+    home-manager.users.wittano = {
+      home = {
+        packages = with pkgs; [ jetbrains.idea-ultimate jdk gradle ];
+        file.".ideavimrc".text = ''
+          set rnu nu
+        '';
+      };
+    };
+
+    systemd.user.services.gradle = {
+      description = ''
+        Gradle - tools to buliding JVM application. Adaptable, fast automation for all
       '';
+      wantedBy = [ "multi-user.target" ];
+      script = "${pkgs.gradle}/bin/gradle --foreground";
+      postStop = "${pkgs.gradle}/bin/gradle --stop";
     };
   };
 }
