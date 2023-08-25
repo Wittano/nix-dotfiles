e04272d9be6adbe6273f54bcf0bb3322af2c9876
Wittano <radoslaw.ratyna@gmail.com>
2023-08-25 19:52:15 +0200

    feat(jvm): added andorid-studio

--- modules/dev/jvm.nix
+++ modules/dev/jvm.nix
@@ -3,6 +3,20 @@ let
   inherit (lib) mkEnableOption mkIf;
 
   cfg = config.modules.dev.jvm;
+  patcherDir = pkgs.writeScriptBin "patcherDir" ''
+    #/usr/bin/env bash
+
+    programs=$(${pkgs.findutils}/bin/find $1 -type f -perm /u=x -exec ${pkgs.file}/bin/file --mime-type {} \; | ${pkgs.gnugrep}/bin/grep -E "application/(x-executable)|(x-pie-executable)" | ${pkgs.coreutils}/bin/cut -d ':' -f 1)
+
+    if [ -z $programs ]; then
+      ${pkgs.coreutils}/bin/echo "Nothing changes"
+    fi
+
+    for p in $programs; do
+      ${pkgs.coreutils}/bin/echo "Patch progam $p"
+      ${pkgs.patchelf}/bin/patchelf --set-interpreter ${pkgs.glibc}/lib/ld-linux-x86-64.so.2 "$p"
+    done
+  '';
 in
 {
   options = {
@@ -16,7 +30,7 @@ in
   config = mkIf cfg.enable {
     home-manager.users.wittano = {
       home = {
-        packages = with pkgs; [ jetbrains.idea-ultimate jdk gradle ];
+        packages = with pkgs; [ jetbrains.idea-ultimate android-studio jdk gradle patcherDir ];
         file.".ideavimrc".text = ''
           set rnu nu
         '';
