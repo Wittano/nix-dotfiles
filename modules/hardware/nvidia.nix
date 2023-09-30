b50e31d516d00d39003d143de4aec06d9849c3c9
Wittano <radoslaw.ratyna@gmail.com>
2023-10-01 01:46:24 +0200

    chore(nvidia): removed obstacle variables

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -3,7 +3,8 @@ let
   inherit (lib) mkEnableOption mkIf;
 
   cfg = config.modules.hardware.nvidia;
-in {
+in
+{
   options = {
     modules.hardware.nvidia = {
       enable = mkEnableOption ''
@@ -29,13 +30,6 @@ in {
       };
 
       nvidia.modesetting.enable = true;
-    };
-
-    environment.variables = {
-      __NV_PRIME_RENDER_OFFLOAD = "1";
-      __NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA-G0";
-      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-      __VK_LAYER_NV_optimus = "NVIDIA_only";
     };
   };
 }
