077766e4f9f4314f554edb4245a0284b3c163b04
Wittano <radoslaw.ratyna@gmail.com>
2023-11-13 10:36:21 +0100

    fix(backup): improved backup script

--- modules/services/backup.nix
+++ modules/services/backup.nix
@@ -20,6 +20,11 @@ in
   };
 
   config = mkIf cfg.enable {
+    systemd.timers.backup.timerConfig = {
+      OnCalendar = "*-*-* *:00:00";
+      Unit = "backup.service";
+      OnUnitActiveSec = "1d";
+    };
     systemd.services.backup = {
       description = ''
         Backup service for ${username} home directory
@@ -28,18 +33,23 @@ in
         Type = "oneshot";
         User = username;
       };
-      startAt = "*-*-* *:00:00";
       wantedBy = [ "multi-user.target" ];
       preStart = ''
         mkdir -p ${cfg.backupDir}
         chown ${username}:users ${cfg.backupDir}
       '';
       script = ''
+        today=$(${pkgs.coreutils}/bin/date -I)
         base_dir=$(${pkgs.coreutils}/bin/dirname ${cfg.backupDir})
-        archive_backup="$base_dir/wittano.backup-$(${pkgs.coreutils}/bin/date -I)".tar
+        archive_backup="$base_dir/wittano.backup-$today.tar"
+
+        if [ -f $archive_backup ]; then
+          ${pkgs.coreutils}/bin/echo "Backup for '$today' has already created"
+          exit 0
+        fi
 
         remove_oldest_backup() {
-          oldest_backup=$(${pkgs.findutils}/bin/find /mnt/backup -name wittano.backup-*.tar -type f -printf '%T+ %p\n' | ${pkgs.coreutils}/bin/sort | ${pkgs.coreutils}/bin/head -1 | ${pkgs.coreutils}/bin/cut -d' ' -f2-)
+          oldest_backup=$(${pkgs.findutils}/bin/find /mnt/backup -maxdepth 1 -name wittano.backup-*.tar -type f -printf '%T+ %p\n' | ${pkgs.coreutils}/bin/sort | ${pkgs.coreutils}/bin/head -1 | ${pkgs.coreutils}/bin/cut -d' ' -f2-)
           if [ -n "$oldest_backup" ]; then
             ${pkgs.coreutils}/bin/rm $oldest_backup
           else
