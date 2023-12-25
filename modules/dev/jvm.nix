a8b4bb1692fa1c2b606eb15e6838c1c5ffd122f6
Wittano <radoslaw.ratyna@gmail.com>
2023-12-25 18:25:24 +0100

    feat(jvm): added new option: enableAndorid

--- modules/dev/jvm.nix
+++ modules/dev/jvm.nix
@@ -10,12 +10,15 @@ in
       enable = mkEnableOption ''
         Enable JVM development enviroment
       '';
+      enableAndroid = mkEnableOption ''
+        Enable Android development enviroment
+      '';
     };
   };
 
   config = mkIf cfg.enable {
     home-manager.users.wittano = {
-      home.packages = with pkgs; [ jetbrains.idea-ultimate android-studio graalvm-ce ];
+      home.packages = with pkgs; [ jetbrains.idea-ultimate graalvm-ce ] ++ (if cfg.enableAndroid then [ andorid-studio ] else [ ]);
 
       programs.fish.shellAliases = mkIf (config.modules.shell.fish.enable) {
         pjvm = "cd $HOME/projects/own/jvm";
