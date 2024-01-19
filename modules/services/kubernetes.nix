0b2916a0ee75b29b86fb03cd4e85653f26f9b4bd
Wittano <radoslaw.ratyna@gmail.com>
2024-01-19 11:03:26 +0100

    fix(kubernetes): added bootstrap-token option for master node

--- modules/services/kubernetes.nix
+++ modules/services/kubernetes.nix
@@ -3,6 +3,9 @@ with lib;
 with builtins;
 let
   cfg = config.modules.services.kubernetes;
+  ipAddress = (head config.networking.interfaces.eno1.ipv4.addresses).address;
+  certFile = "";
+  apiServerPort = 6443;
 in
 {
   options = {
@@ -13,31 +16,39 @@ in
 
   config = mkIf (cfg.enable) {
     home-manager.users.wittano = {
-      home.packages = with pkgs; [ kubectl k9s ];
-      programs.fish.shellAbbrs.kubectl = "sudo kubectl --kubeconfig ~/.kube/config";
+      programs.fish = {
+        shellAbbrs = {
+          kctl = "sudo kubectl --kubeconfig ~/.kube/config";
+          kadm = "sudo kubeadm --kubeconfig ~/.kube/config";
+        };
+        shellAliases = {
+          ktokendel = "sudo kubeadm --kubeconfig ~/.kube/config token list | awk '{print $1}' | grep -E '^([a-z0-9]{6})\\.([a-z0-9]{16})$' | xargs sudo kubeadm --kubeconfig ~/.kube/config token delete";
+        };
+      };
     };
 
-    services.kubernetes =
-      let
-        ipAddress = (head config.networking.interfaces.eno1.ipv4.addresses).address;
-        apiServerPort = 6443;
-      in
-      {
-        roles = [ "master" "node" ];
-        masterAddress = ipAddress;
-        apiserverAddress = "https://${ipAddress}:${toString apiServerPort}";
-        easyCerts = true;
-        apiserver = {
-          securePort = apiServerPort;
-          advertiseAddress = ipAddress;
-        };
+    networking.firewall.allowedTCPPorts = [ apiServerPort ];
 
-        # use coredns
-        addons.dns.enable = true;
+    users.users.wittano.extraGroups = [ "kubernetes" ];
 
-        # needed if you use swap
-        kubelet.extraOpts = "--fail-swap-on=false";
+    services.kubernetes = {
+      roles = [ "master" ];
+      masterAddress = ipAddress;
+      apiserverAddress = "https://${ipAddress}:${toString apiServerPort}";
+      easyCerts = true;
+      apiserver = {
+        securePort = apiServerPort;
+        advertiseAddress = ipAddress;
+        extraOpts = "--enable-bootstrap-token-auth=true";
       };
+      controllerManager.extraOpts = "--controllers=*,bootstrapsigner,tokencleaner";
+
+      # use coredns
+      addons.dns.enable = true;
+
+      # needed if you use swap
+      kubelet.extraOpts = "--fail-swap-on=false";
+    };
   };
 }
 
