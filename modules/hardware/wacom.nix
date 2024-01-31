38bea1845a33e594aeeab5c90a884d4905ac3051
Wittano <radoslaw.ratyna@gmail.com>
2024-01-31 17:35:16 +0100

    feat(wacom): added auto-mapping tablet to primary display

--- modules/hardware/wacom.nix
+++ modules/hardware/wacom.nix
@@ -2,6 +2,16 @@
 with lib;
 let
   cfg = config.modules.hardware.wacom;
+  xsetwacom = "${pkgs.xf86_input_wacom}/bin/xsetwacom";
+  awk = "${pkgs.gawk}/bin/awk";
+  serviceScript = pkgs.writeShellScriptBin "setup-wacom" ''
+    #!/bin/bash
+    devices=$(${xsetwacom} list devices | ${awk} '{print $9}')
+
+    for d in $devices; do
+      ${xsetwacom} --set "$d" MapToOutput HEAD-0
+    done
+  '';
 in
 {
   options = {
@@ -11,22 +21,29 @@ in
   };
 
   config = mkIf cfg.enable {
-    services.xserver.wacom.enable = cfg.enable;
+    services.xserver.wacom.enable = true;
 
     home-manager.users.wittano = {
       home.packages = with pkgs; [ krita ];
-      programs.fish.functions.fixWacom.body =
-        let
-          xsetwacom = "${pkgs.xf86_input_wacom}/bin/xsetwacom";
-          awk = "${pkgs.gawk}/bin/awk";
-        in
-          /*fish*/''
-          set -l devices (${xsetwacom} list devices | ${awk} '{print $9}')
 
-          for d in $devices;
-            ${xsetwacom} --set "$d" MapToOutput HEAD-0
-          end
-        '';
+      systemd.user.services."setup-wacom" = mkIf ((builtins.length config.services.xserver.xrandrHeads) > 1) {
+        Unit.Description = "Map wacom tablet to main display";
+        Service = {
+          Environment = "DISPLAY=:0";
+          Type = "simple";
+          ExecStart = "${serviceScript}/bin/setup-wacom";
+        };
+        Install.WantedBy = [ "graphical.target" ];
+      };
+
+      programs.fish.functions.fixWacom.body =
+        /*fish*/''
+        set -l devices (${xsetwacom} list devices | ${awk} '{print $9}')
+
+        for d in $devices;
+          ${xsetwacom} --set "$d" MapToOutput HEAD-0
+        end
+      '';
     };
   };
 }
