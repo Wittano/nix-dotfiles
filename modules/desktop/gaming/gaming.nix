019c1ebc874b6a5da0141339ece9ab65562d3788
Wittano <radoslaw.ratyna@gmail.com>
2024-02-10 03:57:02 +0100

    refactor(gaming): replaced aliases by creating new script programs

--- modules/desktop/gaming/gaming.nix
+++ modules/desktop/gaming/gaming.nix
@@ -3,6 +3,23 @@ with lib;
 let
   cfg = config.modules.desktop.gaming;
   osu = inputs.nix-gaming.packages.${pkgs.hostPlatform.system}.osu-lazer-bin;
+
+  # TODO Add autodetect compatdata directory for AoE2
+  fixAge2Sync = pkgs.writeScriptBin "fixAge2Sync" /*bash*/
+    ''
+      #!/usr/bin/env bash
+          
+      cd /mnt/gaming/SteamLibrary/steamapps/compatdata/813780/pfx/drive_c/windows/system32
+
+      if [ ! -e "vc_redist.x64.exe" ]; then
+          ${pkgs.wget}/bin/wget "https://aka.ms/vs/16/release/vc_redist.x64.exe"
+      fi
+
+      sudo ${pkgs.cabextract}/bin/cabextract vc_redist.x64.exe
+      sudo ${pkgs.cabextract}/bin/cabextract a10
+    '';
+  fixSteamSystemTray = pkgs.writeScriptBin "fixSteamSystemTray"
+    "rm -rf ~/.local/share/Steam/ubuntu12_32/steam-runtime/pinned_libs_{32,64}";
 in
 {
   options = {
@@ -36,6 +53,10 @@ in
       # Wine
       wineWowPackages.full
 
+      # fix scripts
+      fixSteamSystemTray
+      fixAge2Sync
+
       # FSH
       steam-run
 
@@ -55,30 +76,5 @@ in
         fsType = "ext4";
       };
     };
-
-    home-manager.users.wittano.programs.fish.shellAliases =
-      let
-
-        # TODO Add autodetect compatdata directory for AoE2
-        fixAge2SyncScript = pkgs.writeScriptBin "fixAge2Sync.sh" # bash
-          ''
-            #!/usr/bin/env bash
-          
-            cd /mnt/gaming/SteamLibrary/steamapps/compatdata/813780/pfx/drive_c/windows/system32
-
-            if [ ! -e "vc_redist.x64.exe" ]; then
-                ${pkgs.wget}/bin/wget "https://aka.ms/vs/16/release/vc_redist.x64.exe"
-            fi
-
-            sudo ${pkgs.cabextract}/bin/cabextract vc_redist.x64.exe
-            sudo ${pkgs.cabextract}/bin/cabextract a10
-          '';
-      in
-      {
-        fixSteamSystemTray =
-          "rm -rf ~/.local/share/Steam/ubuntu12_32/steam-runtime/pinned_libs_{32,64}";
-        fixAge2Sync = "${pkgs.bash}/bin/bash ${fixAge2SyncScript}";
-      };
   };
-
 }
