59b4a757594a7669a8c73c9a57fed8b18b137010
Wittano <radoslaw.ratyna@gmail.com>
2024-02-11 12:19:56 +0100

    feat(picom): migrated config from file to NixOS files

--- modules/desktop/apps/picom.nix
+++ modules/desktop/apps/picom.nix
@@ -1,28 +1,61 @@
-{ pkgs, lib, cfg, dotfiles, ... }:
+{ pkgs, lib, cfg, ... }:
 with lib;
 with lib.my; {
-  home-manager.users.wittano = {
-    home.activation.linkMutablePicomConfig =
-      link.createMutableLinkActivation cfg ".config/picom";
+  services.picom = {
+    enable = true;
+    package = pkgs.picom-allusive;
+    backend = "glx";
+    fade = true;
+    fadeDelta = 4;
 
-    xdg.configFile = mkIf (cfg.enableDevMode == false) {
-      picom.source = dotfiles.picom.source;
+    vSync = true;
+
+    wintypes = {
+      dock.shadow = false;
+      notification.corner-radius = 5;
     };
 
-    systemd.user.services.picom = {
-      Unit = {
-        Description = ''
-          A lightweight compositor for X11 (previously a compton fork)
-        '';
-        After = "graphical-session.target";
-      };
-      Service = {
-        ExecStart = ''
-          ${pkgs.picom-jonaburg}/bin/picom --experimental-backends
-        '';
-        Restart = "on-failure";
-      };
-      Install = { WantedBy = [ "graphical-session.target" ]; };
+    opacityRules = [
+      "95:class_g = 'Alacritty' && focused"
+      "85:class_g = 'Alacritty' && !focused"
+      "95:class_g = 'kitty' && focused"
+      "85:class_g = 'kitty' && !focused"
+    ];
+
+    settings = {
+      glx-no-stencil = true;
+      glx-copy-from-front = false;
+
+      # Border
+      corner-radius = 10;
+      round-borders = 1;
+      rounded-corners-exclude = [
+        "window_type = 'dock'"
+      ];
+
+      # Blur
+      blur-background = true;
+      blur-method = "gaussian";
+      blur-strength = 6;
+      blur-background-exclude = [
+        "window_type = 'dock'"
+      ];
+
+      # Fade
+      no-fading-openclose = false;
+
+      # Other
+      mark-wmwin-focused = true;
+      mark-ovredir-focused = true;
+      detect-rounded-corners = true;
+      detect-client-opacity = true;
+      refresh-rate = 0;
+      dbe = false;
+      unredir-if-possible = false;
+      detect-transient = true;
+      detect-client-leader = true;
+      use-damage = true;
+      xrender-sync-fence = true;
     };
   };
 }
