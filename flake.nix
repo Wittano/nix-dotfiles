0921b4e3e6862a796356423ca8f1134b5fd9b031
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2024-02-19 03:05:23 +0100

    chore(flake): added nixpkgs source for gaming flakes

--- flake.nix
+++ flake.nix
@@ -28,7 +28,10 @@
       url = "github:ezKEa/aagl-gtk-on-nix";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
-    nix-gaming.url = "github:fufexan/nix-gaming";
+    nix-gaming = {
+      url = "github:fufexan/nix-gaming";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
     agenix.url = "github:ryantm/agenix";
     nixvim = {
       url = "github:nix-community/nixvim?ref=nixos-23.11";
