23c5a135577feb1517fbb41e9abb802f276c7aa6
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2024-02-22 22:48:19 +0100

    fix(nitogen): removed dobule quotes around file paths from generated cfg files

--- modules/desktop/apps/nitrogen.nix
+++ modules/desktop/apps/nitrogen.nix
@@ -2,26 +2,64 @@
 with lib;
 with lib.my;
 let
-  tomlGenerator = pkgs.formats.toml { };
+  toCfg = name: value:
+    let
+      # This script fix problem for nitrogen, becauses for some resons
+      # nitrogen read path with " chars and throws exception for that
+      fixCfgPaths = builtins.toFile "fixCfgPaths.py" /*python*/''
+        #!/usr/bin/env python
+        from typing import List
+        import sys
+
+        if len(sys.argv) < 2:
+          raise AssertionError("Missing required input file")
+
+        content: List[str] = []
+        inputFile = sys.argv[1]
+
+        with open(inputFile, "r") as f:
+            for line in f:
+                content.append(line)
+
+        if len(content) <= 0:
+            raise AssertionError("file is empty")
+
+        with open(inputFile, "w") as f:
+            for i, line in enumerate(content):
+                if "/" in line:
+                    content[i] = line.replace("\"", "")
+
+            f.writelines(content)
+      '';
+    in
+    ((pkgs.formats.toml { }).generate name value).overrideAttrs
+      (final: prev: {
+        nativeBuildInputs = prev.nativeBuildInputs ++ [ pkgs.python3 ];
+        buildCommand = /*bash*/ ''
+          json2toml "$valuePath" $out
+          python ${fixCfgPaths} $out
+        '';
+      });
 in
 {
   home-manager.users.wittano = {
     home.packages = with pkgs; [ nitrogen ];
 
     xdg.configFile = mkIf (cfg.enableDevMode == false) {
-      "nitrogen/bg-saved.cfg".source = tomlGenerator.generate "bg-saved.cfg" {
-        xin_1 = {
-          file = dotfiles.wallpapers."11.jpeg".source;
-          mode = 0;
-          bgcolor = "#000000";
+      "nitrogen/bg-saved.cfg".source = toCfg "bg-saved.cfg"
+        {
+          xin_1 = {
+            file = dotfiles.wallpapers."11.jpeg".source;
+            mode = 0;
+            bgcolor = " #000000";
+          };
+          xin_0 = {
+            file = dotfiles.wallpapers."33.png".source;
+            mode = 0;
+            bgcolor = "#000000";
+          };
         };
-        xin_0 = {
-          file = dotfiles.wallpapers."33.png".source;
-          mode = 0;
-          bgcolor = "#000000";
-        };
-      };
-      "nitrogen/nitrogen.cfg".source = tomlGenerator.generate "nitrogen.cfg" {
+      "nitrogen/nitrogen.cfg".source = toCfg "nitrogen.cfg" {
         geometry = {
           posx = 286;
           posy = 112;
