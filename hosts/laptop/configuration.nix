893a9ac90bde41518ca6fee47b54da81c7ee79ae
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2024-04-03 06:00:28 +0200

    feat(laptop): updated config

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -2,32 +2,24 @@
 
   imports = [ ./hardware.nix ./networking.nix ];
 
-  modules =
-    let
-      enableWithDevMode = {
-        enable = true;
-        enableDevMode = isDevMode;
-      };
-    in
-    {
-      desktop = {
-        apps.enable = true;
-        gnome.enable = true;
-      };
-      dev.ide.list = [ "go" ];
-      hardware = {
-        grub.enable = true;
-        virtualization.enable = true;
-        docker.enable = true;
-        nvidia.enable = true;
-      };
-      services = {
-        backup = {
-          enable = true;
-          backupDir = "/mnt/backup/wittano.nixos";
-        };
-        syncthing.enable = true;
-      };
+  modules = {
+    desktop = {
+      apps.enable = true;
+      gnome.enable = true;
     };
+    dev.ide.list = [ "go" ];
+    hardware = {
+      grub.enable = true;
+      docker.enable = true;
+      nvidia.enable = true;
+    };
+    services = {
+      backup = {
+        enable = true;
+        backupDir = "/mnt/backup/wittano.nixos";
+      };
+      syncthing.enable = true;
+    };
+  };
 
 }
