1348fbb9d94139635a66c4249b5d91e5188f9490
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2024-04-14 11:13:49 +0200

    feat(honkai-railyway-grub-theme): added assert condition for theme name

--- pkgs/grub/honkai-railway/default.nix
+++ pkgs/grub/honkai-railway/default.nix
@@ -1,25 +1,41 @@
 { stdenvNoCC
 , lib
 , fetchFromGitHub
-, name ? "Dr.Ratio"
+, theme ? "Dr.Ratio"
 }:
+
+assert builtins.any (x: x == theme) [
+  "Acheron"
+  "Argenti"
+  "BlackSwan"
+  "Dr.Ratio"
+  "Hanya"
+  "Huohuo"
+  "Luocha"
+  "RuanMei"
+  "Sparkle"
+  "Acheron_cn"
+  "Argenti_cn"
+  "BlackSwan_cn"
+  "Dr.Ratio_cn"
+  "Hanya_cn"
+  "Huohuo_cn"
+  "Luocha_cn"
+  "RuanMei_cn"
+  "Sparkle_cn"
+];
+
 stdenvNoCC.mkDerivation {
   name = "StarRailGrubThemes";
+
   src = fetchFromGitHub {
     owner = "voidlhf";
     repo = "StarRailGrubThemes";
-    rev = "b3634c646556bb14d82e04233c63afffd9235420";
+    rev = "41a004ca19dc52bc916227c953197ec11097e1fd";
     sha256 = "sha256-lcZ21QViH+cW6G1g/KpJwo9ZfWNm5+fZnpDELDrgCyY=";
   };
 
-  installPhase = /*bash*/ ''
-    if [ "${name}" == "*" ]; then
-      echo "StarRailGrubThemes doesn't allow to copy all themes at the same time"
-      exit 1
-    fi
-
-    cp -r ./assets/themes/${name} $out
-  '';
+  installPhase = "cp -r ./assets/themes/${theme} $out";
 
   meta = with lib; {
     homepage = "https://github.com/voidlhf/StarRailGrubThemes";
