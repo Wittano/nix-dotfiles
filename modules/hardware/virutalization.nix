ccc8cceca6eaf872b84e010214af31e48199ccf5
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2024-04-16 19:38:59 +0200

    refactor(virtualization): moved fish complietion to Nix

--- modules/hardware/virutalization.nix
+++ modules/hardware/virutalization.nix
@@ -110,7 +110,21 @@ in
 
     modules.shell.fish.completions = mkIf (cfg.enableVagrant) [{
       name = "vm";
-      value = dotfiles.fish.completions."vm.fish".source;
+      value = ''
+        function __fish_vm_list
+          find $HOME/projects/config/system -name Vagrantfile -type f | cut -d '/' -f 8
+        end
+
+        set -l vagrant_commands destroy start halt up ssh reload restart
+
+        for vm in (__fish_vm_list)
+          complete -f -c vm -n "not __fish_seen_subcommand_from (__fish_vm_list)" -a $vm
+          complete -f -c vm -n "__fish_seen_subcommand_from $vm; and not __fish_seen_subcommand_from $vagrant_commands" -a "$vagrant_commands"
+        end
+
+        complete -f -c vm -n "not __fish_seen_subcommand_from all" -a all
+        complete -f -c vm -n "__fish_seen_subcommand_from all; and not __fish_seen_subcommand_from $vagrant_commands" -a "$vagrant_commands"
+      '';
     }];
 
     home-manager.users.wittano = mkIf (cfg.enableVagrant) {
