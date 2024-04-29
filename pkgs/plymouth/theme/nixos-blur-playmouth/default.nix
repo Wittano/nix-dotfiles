3b109ba947bd06c4d12bd86ca158ca2d59f032c8
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2024-04-29 07:50:43 +0200

    fix(nixos-blur): added source code directly into repository
    I had to do, because git website with official repository was gone

--- pkgs/plymouth/theme/nixos-blur-playmouth/default.nix
+++ pkgs/plymouth/theme/nixos-blur-playmouth/default.nix
@@ -1,25 +1,20 @@
-{ lib, stdenv, fetchFromGitea }: stdenv.mkDerivation {
+{ lib, stdenv }: stdenv.mkDerivation {
   name = "nixos-blur-plymouth";
 
-  src = fetchFromGitea {
-    domain = "git.gurkan.in";
-    owner = "gurkan";
-    repo = "nixos-blur-plymouth";
-    rev = "ea75b51a1f04aa914647a2929eab6bbe595bcfc0";
-    sha256 = "sha256-BSmh+Gy3yJMA4RoJ0uaQ/WsYBs+Txr6K3cAQjf+yM5Y=";
-  };
+  src = ./nixos-blur;
 
   installPhase = ''
     mkdir -p $out/share/plymouth/themes/nixos-blur
     
-    cp -r ./nixos-blur/* $out/share/plymouth/themes/nixos-blur
+    cp -r * $out/share/plymouth/themes/nixos-blur
     
     chmod +x $out/share/plymouth/themes/nixos-blur/nixos-blur.plymouth
     chmod +x $out/share/plymouth/themes/nixos-blur/nixos-blur.script
   '';
 
   meta = with lib; {
-    homepage = "https://git.gurkan.in/gurkan/nixos-blur-plymouth";
+    # Offical git website with source was gone. I had to move package into my repository
+    # homepage = "https://git.gurkan.in/gurkan/nixos-blur-plymouth";
     description = "Blue plymouth theme created by gurkan";
     license = licenses.gpl3;
     maintainers = with maintainers; [ Wittano ];
