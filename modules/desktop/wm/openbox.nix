faf8c80edc725c12f3db56325cec5ba2543b51e0
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2024-05-03 09:42:16 +0200

    refactor(openbox): migrated dracula theme into config

--- modules/desktop/wm/openbox.nix
+++ modules/desktop/wm/openbox.nix
@@ -1,6 +1,18 @@
 { config, pkgs, lib, dotfiles, isDevMode, hostname, ... }:
 with lib;
 with lib.my;
+let
+  draculaOpenbox = pkgs.fetchFromGitHub {
+    owner = "dracula";
+    repo = "openbox";
+    rev = "b3222509bb291dc62d201a66a1547a7aac0040b3";
+    sha256 = "sha256-GZ6/ThHBP3TZshDPHdsNjQEpqowt4eqva0MI/mzELRg=";
+  };
+  draculaIcon = pkgs.fetchzip {
+    url = "https://github.com/dracula/gtk/files/5214870/Dracula.zip";
+    sha256 = "sha256-rcSKlgI3bxdh4INdebijKElqbmAfTwO+oEt6M2D1ls0=";
+  };
+in
 desktop.mkDesktopModule {
   inherit config isDevMode hostname dotfiles;
 
@@ -23,19 +35,27 @@ desktop.mkDesktopModule {
   # TODO Update config
   extraConfig = { activationPath, ... }: {
     home-manager.users.wittano = {
-      home.packages = with pkgs; [
-        openbox-menu
-        lxmenu-data
-        obconf
-        volumeicon
-        gsimplecal
-        tint2
-        # Utils
-        arandr
-      ];
+      home = {
+        packages = with pkgs; [
+          openbox-menu
+          lxmenu-data
+          obconf
+          volumeicon
+          gsimplecal
+          tint2
+          # Utils
+          arandr
+        ];
+        file = {
+          ".themes/Dracula-withoutBorder".source = draculaOpenbox + "/Dracula-withoutBorder";
+          ".icons/dracula".source = draculaIcon;
+        };
+      };
 
       xdg.configFile."openbox/autostart".source = activationPath;
     };
+
+    environment.systemPackages = with pkgs; [ dracula-theme ];
 
     services.xserver = {
       enable = true;
