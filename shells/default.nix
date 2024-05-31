62ad8f923ab935423d53d4297a2d150362cf5a69
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2024-05-31 21:00:13 +0200

    feat(shell): added auto-generated dry-run commonad for hosts

--- shells/default.nix
+++ shells/default.nix
@@ -3,8 +3,33 @@
 , nil
 , shellcheck
 , shfmt
+, lib
+, nixos-rebuild
+, writeShellApplication
 , ...
 }:
+with lib;
+let
+  desktops = trivial.pipe ./../modules/desktop/wm [
+    builtins.readDir
+    builtins.attrNames
+    (builtins.map (x: strings.removeSuffix ".nix" x))
+  ];
+  hosts = trivial.pipe ./../hosts [
+    builtins.readDir
+    builtins.attrNames
+  ];
+  confs = lists.flatten (builtins.map (host: builtins.map (desktop: "${host}-${desktop}") desktops) hosts);
+  allConfigs = confs ++ builtins.map (x: "${x}-dev") confs;
+
+  tryCommands = builtins.map
+    (x: writeShellApplication {
+      name = "try-${x}";
+      runtimeInputs = [ nixos-rebuild ];
+      text = "nixos-rebuild dry-build --flake .#${x} --show-trace";
+    })
+    allConfigs;
+in
 mkShell {
   nativeBuildInputs = [
     # Nix
@@ -14,5 +39,5 @@ mkShell {
     # Bash
     shellcheck
     shfmt
-  ];
+  ] ++ tryCommands;
 }
