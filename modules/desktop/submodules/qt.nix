54c54650cb40c6ef6e672c0335fe4aeeab47af5f
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2024-06-01 18:53:23 +0200

    fix(qt): problem with kvantum theme
    Missing environment variables for Qt themes were cause why Qt didn't
    load kvantum theme

--- modules/desktop/submodules/qt.nix
+++ modules/desktop/submodules/qt.nix
@@ -1,11 +1,51 @@
 { pkgs, lib, ... }:
 with lib;
 with lib.my;
+let
+  catppuccinTheme = pkgs.fetchFromGitHub {
+    repo = "Kvantum";
+    owner = "catppuccin";
+    rev = "04be2ad3d28156cfb62478256f33b58ee27884e9";
+    sha256 = "sha256-apOPiVwePXbdKM1/0HAfHzIqAZxvfgL5KHzhoIMXLqI=";
+  };
+
+  themes = attrsets.mapAttrs'
+    (n: _: {
+      name = "Kvantum/${n}";
+      value = {
+        source = catppuccinTheme + "/src/${n}";
+      };
+    })
+    (builtins.readDir (catppuccinTheme + "/src"));
+in
 {
   config = {
     qt = {
       enable = true;
-      style = "adwaita-dark";
+      platformTheme = "qt5ct";
+      style = "kvantum";
+    };
+
+    home-manager.users.wittano = {
+      home = {
+        packages = with pkgs; [ kdePackages.qt6ct libsForQt5.qt5ct ];
+        sessionVariables = {
+          QT_QPA_PLATFORMTHEME = "qt5ct";
+          QT_QPA_PLATFORM = "xcb";
+        };
+      };
+
+      qt = {
+        enable = true;
+        platformTheme.name = "qtct";
+        style.name = "kvantum";
+      };
+
+      xdg.configFile = themes // {
+        "Kvantum/kvantum.kvconfig".source = (pkgs.formats.ini { }).generate "kvantum.kvconfig" {
+          General.theme = "Catppuccin-Frappe-Sky";
+        };
+      };
     };
 
     environment.systemPackages = with pkgs.libsForQt5; [ breeze-icons ];
