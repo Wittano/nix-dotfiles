8e0717082ba0179b18a5f4648be307c373186bfd
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2024-07-21 02:08:36 +0200

    fix(picom): fixed intend

--- modules/desktop/submodules/picom.nix
+++ modules/desktop/submodules/picom.nix
@@ -27,65 +27,65 @@ with lib;
 
       serviceConfig.ExecCondition = "${pkgs.toybox}/bin/pgrep --ignore-case steam";
     };
-  };
 
-  services.picom = {
-    enable = true;
-    package = pkgs.picom-allusive;
-    backend = "glx";
-    fade = true;
-    fadeDelta = 4;
+    services.picom = {
+      enable = true;
+      package = pkgs.picom-allusive;
+      backend = "glx";
+      fade = true;
+      fadeDelta = 4;
 
-    vSync = true;
+      vSync = true;
 
-    wintypes = {
-      dock.shadow = false;
-      notification.corner-radius = 5;
-    };
+      wintypes = {
+        dock.shadow = false;
+        notification.corner-radius = 5;
+      };
 
-    opacityRules = [
-      "95:class_g = 'Alacritty' && focused"
-      "85:class_g = 'Alacritty' && !focused"
-      "95:class_g = 'kitty' && focused"
-      "85:class_g = 'kitty' && !focused"
-    ];
-
-    settings = {
-      glx-no-stencil = true;
-      glx-copy-from-front = false;
-
-      # Border
-      corner-radius = 10;
-      corners-rule = [
-        "10:class_g = 'xmobar'"
-      ];
-      round-borders = 3;
-
-      # Blur
-      blur-background = true;
-      blur-method = "gaussian";
-      blur-strength = 6;
-      blur-background-exclude = [
-        "window_type = 'dock'"
+      opacityRules = [
+        "95:class_g = 'Alacritty' && focused"
+        "85:class_g = 'Alacritty' && !focused"
+        "95:class_g = 'kitty' && focused"
+        "85:class_g = 'kitty' && !focused"
       ];
 
-      dbus = true;
+      settings = {
+        glx-no-stencil = true;
+        glx-copy-from-front = false;
 
-      # Fade
-      no-fading-openclose = false;
+        # Border
+        corner-radius = 10;
+        corners-rule = [
+          "10:class_g = 'xmobar'"
+        ];
+        round-borders = 3;
 
-      # Other
-      mark-wmwin-focused = true;
-      mark-ovredir-focused = true;
-      detect-rounded-corners = true;
-      detect-client-opacity = true;
-      refresh-rate = 0;
-      dbe = false;
-      unredir-if-possible = false;
-      detect-transient = true;
-      detect-client-leader = true;
-      use-damage = true;
-      xrender-sync-fence = true;
+        # Blur
+        blur-background = true;
+        blur-method = "gaussian";
+        blur-strength = 6;
+        blur-background-exclude = [
+          "window_type = 'dock'"
+        ];
+
+        dbus = true;
+
+        # Fade
+        no-fading-openclose = false;
+
+        # Other
+        mark-wmwin-focused = true;
+        mark-ovredir-focused = true;
+        detect-rounded-corners = true;
+        detect-client-opacity = true;
+        refresh-rate = 0;
+        dbe = false;
+        unredir-if-possible = false;
+        detect-transient = true;
+        detect-client-leader = true;
+        use-damage = true;
+        xrender-sync-fence = true;
+      };
     };
   };
 }
