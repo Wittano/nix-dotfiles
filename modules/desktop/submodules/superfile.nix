a2f37df8507980b97ea7e0544bd852d5ff75ddde
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2024-07-28 11:40:57 +0200

    fix(superfile): open superfile in current dictionary
    Previous file manager(ranger) opens in currect dictionary, but default
    superfile command opens in $HOME dictionary. For me, it isn't feature,
    so I fixed it

--- modules/desktop/submodules/superfile.nix
+++ modules/desktop/submodules/superfile.nix
@@ -3,6 +3,6 @@
   config = {
     environment.systemPackages = with pkgs; [ superfile ];
 
-    home-manager.users.wittano.programs.fish.shellAliases.ra = "superfile";
+    home-manager.users.wittano.programs.fish.shellAliases.ra = "superfile .";
   };
 }
