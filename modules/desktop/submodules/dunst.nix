e185b17bf78b3a807d0fba0e97a17334819bb0bc
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2024-08-17 20:13:34 +0200

    fix(dunst): set up path to catppuccin config path

--- modules/desktop/submodules/dunst.nix
+++ modules/desktop/submodules/dunst.nix
@@ -1,11 +1,13 @@
-{ ... }:
+{ config, ... }:
 {
   config = {
-    home-manager.users.wittano.services.dunst = {
+    home-manager.users.wittano.services.dunst = rec {
       enable = true;
+      configFile = "${config.home-manager.users.wittano.xdg.configHome}/dunst/dunstrc.d/${catppuccin.prefix}-catppuccin.conf";
       catppuccin = {
         enable = true;
         flavor = "macchiato";
+        prefix = "01";
       };
     };
   };
