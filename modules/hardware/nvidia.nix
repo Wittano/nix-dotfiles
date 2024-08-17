1f31ba22a37d3b6028beda98743731def16ed679
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2024-08-17 11:30:05 +0200

    fix: update dunst and nvidia configuration after update
    - dusnt - catppuccin change localization of config file
    - nvidia - doesn't compile on xamond kernel so I force myself to use
    default stable kernel

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -16,7 +16,10 @@ in
 
     services.xserver.videoDrivers = mkIf config.services.xserver.enable [ "nvidia" ];
 
-    boot.blacklistedKernelModules = [ "nouveau" ];
+    boot = {
+      blacklistedKernelModules = [ "nouveau" ];
+      kernelPackages = mkForce pkgs.linuxPackages;
+    };
 
     environment.sessionVariables.VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
 
