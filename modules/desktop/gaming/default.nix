74a4d3bd98565e1e8a55040b1bd68d3ff033c1c9
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2024-08-30 23:52:30 +0200

    fix(gaming): add fix-gaming-disk script

--- modules/desktop/gaming/default.nix
+++ modules/desktop/gaming/default.nix
@@ -1,9 +1,20 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 with lib;
 let
   cfg = config.modules.desktop.gaming;
   homeDir = config.home-manager.users.wittano.home.homeDirectory;
   steamGamingDir = if cfg.disk.enable then cfg.disk.path else "${homeDir}/.steam/steam";
+  gamingDisk = "/dev/disk/by-label/GAMING";
+
+  fixGamingDisk = pkgs.writeShellApplication {
+    name = "fix-gaming-disk";
+    runtimeInputs = with pkgs; [ sudo ntfs3g ];
+    text = ''
+      sudo umount ${gamingDisk}
+      sudo ntfsfix ${gamingDisk}
+      sudo mount -a
+    '';
+  };
 in
 {
   options = {
@@ -21,13 +32,16 @@ in
   };
 
   config = mkIf cfg.enable {
-    home-manager.users.wittano.gtk.gtk3.bookmarks = mkIf cfg.disk.enable [ "file://${steamGamingDir} Gaming" ];
+    home-manager.users.wittano = {
+      gtk.gtk3.bookmarks = mkIf cfg.disk.enable [ "file://${steamGamingDir} Gaming" ];
+      home.packages = [ fixGamingDisk ];
+    };
 
     boot.supportedFilesystems.ntfs = mkForce cfg.disk.enable;
 
     fileSystems = mkIf cfg.disk.enable {
       "${steamGamingDir}" = {
-        device = "/dev/disk/by-label/GAMING";
+        device = gamingDisk;
         fsType = "ntfs-3g";
         options = [ "rw" "uid=1000" ];
       };
