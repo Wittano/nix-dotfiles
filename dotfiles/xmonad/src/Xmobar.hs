dd8d3d22db1322bcf5bab4fc70832322679ef558
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2024-09-14 11:04:30 +0200

    feat(xmonad): change theme to catppuccin latte

--- dotfiles/xmonad/src/Xmobar.hs
+++ dotfiles/xmonad/src/Xmobar.hs
@@ -4,13 +4,12 @@ import XMonad
 import XMonad.Hooks.StatusBar.PP
 import XMonad.Util.Loggers (logTitles)
 
--- TODO Replace xmobar by polybar or tint2
 bar :: PP
 bar =
   def
     { ppSep = " ",
       ppTitleSanitize = xmobarStrip,
-      ppCurrent = wrap "[ " " ]" . xmobarBorder "" "#a5adcb" 0,
+      ppCurrent = wrap "[ " " ]" . xmobarBorder "" "#ea76cb" 0,
       ppHidden = white . wrap " " "",
       ppHiddenNoWindows = inactive . wrap " " "",
       ppUrgent = urgent . wrap (yellow "!") (yellow "!"),
@@ -24,9 +23,9 @@ bar =
     ppWindow :: String -> String
     ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
 
-    blue = xmobarColor "#8be9fd" ""
-    white = xmobarColor "#cad3f5" ""
-    yellow = xmobarColor "#eed49f" ""
-    urgent = xmobarColor "#ed8796" ""
-    green = xmobarColor "#a6da95" ""
+    blue = xmobarColor "#1e66f5" ""
+    white = xmobarColor "#4c4f69" ""
+    yellow = xmobarColor "#df8e1d" ""
+    urgent = xmobarColor "#d20f39" ""
+    green = xmobarColor "#40a02b" ""
     inactive = xmobarColor "#5b6078" ""
