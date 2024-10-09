dc6706d9f5c33aea3bd8179b92b5230bb09463c6
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2024-10-09 23:27:25 +0200

    feat(apps): revert discord app to config

--- modules/desktop/submodules/general.nix
+++ modules/desktop/submodules/general.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, unstable, ... }:
+{ pkgs, lib, unstable, master, ... }:
 with lib;
 with lib.my;
 let
@@ -68,7 +68,7 @@ in
         # telegram-desktop
         fixedSignal # Signal desktop
         # element-desktop # matrix communicator
-        # master.vesktop
+        master.vesktop
         # irssi # IRC chat
         logseq
       ];
