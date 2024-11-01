528c217350f0ed0cc8dd7d84e6d076c18e1b4598
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2024-11-01 12:09:42 +0100

    feat(syncthing): added Work device and sharing learning project
    I added device to share my projects/learning staff which I created for
    enterprice purposes

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -12,7 +12,9 @@ in
     };
   };
 
-  config = {
+  config = rec {
+    modules.dev.lang.ides = mkIf (cfg.enable && services.syncthing.settings.devices ? Work) [ "jvm" ];
+
     services.syncthing = {
       enable = cfg.enable;
       systemService = true;
@@ -28,6 +30,21 @@ in
             devices = [
               "TrueNAS"
             ];
+          };
+          learning = {
+            id = "7uub2-oxvra";
+            label = "Enterprice learning projects";
+            path = "~/projects/learning";
+            devices = [
+              "Work"
+            ];
+            versioning = {
+              type = "trashcan";
+              params = {
+                keep = "3";
+                cleanoutDays = "356";
+              };
+            };
           };
           sync = {
             id = "default";
@@ -60,8 +77,8 @@ in
           TrueNAS.id = "CIMVMQO-7RLKQAL-BXRS6Z3-XXFPRLB-PYHZUR3-KKH5HGX-PFWLY6S-C3KLEQ6";
           Karol.id = "F7EH7MZ-N5VYRKT-IA2XWJG-I7SPGDP-RDVSZCU-WTCI534-NQPF7I2-KLE6IQL";
           Win10VM.id = "4KACCLO-MJCLRTA-2TOCUIE-4CNJL3W-EWUDXIR-2MIPHNC-HMZPQFP-IYTOHAW";
+          Work.id = "M3EUKVC-IYHSZZF-OFX75LZ-3E4WZAJ-PGUTYXD-FYDZSEW-GRBGRDZ-IBOHGQK";
         };
-        extraOptions.gui.theme = "dark";
       };
     };
   };
