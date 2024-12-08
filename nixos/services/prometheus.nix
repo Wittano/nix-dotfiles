17cf6b56b936e98fec1e2ab73adadb76249385dc
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2024-12-08 02:20:29 +0100

    feat(win10): added windows_exporter to prometheus

--- nixos/services/prometheus.nix
+++ nixos/services/prometheus.nix
@@ -1,20 +1,32 @@
 { config, lib, ... }:
 with lib;
 with lib.my;
+let
+  windowsExporterConfig = attrsets.optionalAttrs config.hardware.virtualization.wittano.enableWindowsVM
+    {
+      job_name = "windows_exporter";
+      static_configs = [{
+        targets = [ "192.168.122.110:9182" ];
+      }];
+    };
+in
 {
   options.services.prometheus.wittano.enable = mkEnableOption "prometheus service";
 
   config = mkIf config.services.prometheus.wittano.enable {
     networking.firewall.interfaces.eno1.allowedTCPPorts = [ 9090 9100 ];
 
-    services.prometheus = {
+    services.prometheus = rec {
       enable = true;
       port = 9090;
 
-      scrapeConfigs = [{
-        job_name = "node";
-        static_configs = [{ targets = [ "localhost:9100" ]; }];
-      }];
+      scrapeConfigs = [
+        {
+          job_name = "node";
+          static_configs = [{ targets = [ "localhost:${builtins.toString exporters.node.port}" ]; }];
+        }
+        windowsExporterConfig
+      ];
 
       exporters.node = {
         enable = true;
