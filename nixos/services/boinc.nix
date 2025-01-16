a3bb00d171700832d3688d7a2e9f628374de0304
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2025-01-16 22:04:53 +0100

    fix(boinc): added all users to boinc group
    I forgot add 'wito' user to boinc group

--- nixos/services/boinc.nix
+++ nixos/services/boinc.nix
@@ -1,5 +1,6 @@
 { config, pkgs, lib, ... }:
 with lib;
+with lib.my;
 let
   nvidiaDriverPackage = lists.optionals config.hardware.nvidia.enable [ config.boot.kernelPackages.nvidiaPackages.stable ];
   extraEnvPackages = with pkgs; [ ocl-icd ] ++ nvidiaDriverPackage;
@@ -8,7 +9,9 @@ in
   options.services.boinc.wittano.enable = mkEnableOption "Enable BOINC deamon";
 
   config = mkIf config.services.boinc.wittano.enable {
-    users.users.wittano.extraGroups = [ "boinc" ];
+    users.users = desktop.mkMultiUserHomeManager [ "wittano" "wito" ] {
+      extraGroups = [ "boinc" ];
+    };
 
     hardware.virtualization.wittano.stopServices = [{
       name = "win10";
