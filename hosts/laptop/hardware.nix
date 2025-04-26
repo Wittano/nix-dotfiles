45536f811d32143e51ec48d528e3639be1509e9a
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2025-04-26 08:10:36 +0200

    feat(laptop): added LUKS support for laptop disks

--- hosts/laptop/hardware.nix
+++ hosts/laptop/hardware.nix
@@ -12,6 +12,18 @@ _: {
       device = "/dev/disk/by-label/NIX_STORE";
       fsType = "ext4";
     };
+    "/mnt/backup" = {
+      device = "/dev/disk/by-label/BACKUP";
+      fsType = "btrfs";
+    };
+  };
+
+  boot = {
+    loader.efi.canTouchEfiVariables = true;
+    initrd.luks.devices = {
+      cryptroot.device = "/dev/disk/by-uuid/fcEuNQ-0SCr-dfKI-9ppi-ZHIS-OUtP-gaWpMo";
+      crypthdd.device = "/dev/disk/by-uuid/9M8Fd2-Dttq-mRyD-u0BC-hz3z-JYWM-jTEwlH";
+    };
   };
 
   swapDevices = [
