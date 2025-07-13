741751a05078a835393bd455ff974ab3013e8377
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2025-07-13 18:55:21 +0200

    feat(laptop): changed partion layout

--- hosts/laptop/hardware.nix
+++ hosts/laptop/hardware.nix
@@ -12,8 +12,8 @@ _: {
       device = "/dev/disk/by-label/NIX_STORE";
       fsType = "ext4";
     };
-    "/mnt/backup" = {
-      device = "/dev/disk/by-label/BACKUP";
+    "/home" = {
+      device = "/dev/disk/by-label/HOME";
       fsType = "btrfs";
     };
   };
@@ -25,13 +25,8 @@ _: {
       efi.canTouchEfiVariables = true;
     };
     initrd.luks.devices = {
-      cryptroot = {
-        device = "/dev/disk/by-uuid/4cb5d676-9a9d-4cdc-84b6-0156f0ae6a9c";
-        preLVM = true;
-        bypassWorkqueues = true;
-      };
-      crypthdd = {
-        device = "/dev/disk/by-uuid/492227ea-4ab0-4dee-9cea-cf9d89e5bb6f";
+      crypthome = {
+        device = "/dev/disk/by-label/HOME";
         preLVM = true;
       };
     };
