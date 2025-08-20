f05fad9255c2a94013e72ccdb53ebb162557209b
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2025-08-20 18:42:36 +0200

    feat(disks): migrated /nix and /home dictionary to a new disk partitions

--- hosts/pc/hardware.nix
+++ hosts/pc/hardware.nix
@@ -4,6 +4,14 @@ _: {
       device = "/dev/disk/by-label/ROOT";
       fsType = "ext4";
     };
+    "/home" = {
+      device = "/dev/disk/by-label/NIX_HOME";
+      fsType = "ext4";
+    };
+    "/nix" = {
+      device = "/dev/disk/by-label/NIX_STORE";
+      fsType = "ext4";
+    };
     "/boot/efi" = {
       device = "/dev/disk/by-label/BOOT";
       fsType = "vfat";
