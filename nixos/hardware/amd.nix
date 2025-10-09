05ad952827a05f204bb3c72835614975a0f1cf4b
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2025-10-09 12:51:19 +0200

    feat(amd): enabled VRR support to AMDGPU
    I added VVR(Variable refresh rate) for my GPU cuz It could improve
    general performence on my machine

--- nixos/hardware/amd.nix
+++ nixos/hardware/amd.nix
@@ -8,7 +8,17 @@ in
   options.hardware.amd.enable = mkEnableOption "Enable AMD drivers";
 
   config = mkIf cfg.enable {
-    services.xserver.videoDrivers = mkIf config.services.xserver.enable [ "amdgpu" ];
+    services.xserver = {
+      videoDrivers = mkIf config.services.xserver.enable [ "amdgpu" ];
+      config = mkIf (config.home-manager.users.wittano.programs.games.enable || config.programs.steam.enable) ''
+        Section "OutputClass"
+         Identifier "AMD"
+         MatchDriver "amdgpu"
+         Driver "amdgpu"
+         Option "VariableRefresh" "true"
+        EndSection
+      '';
+    };
 
     boot.initrd.kernelModules = [ "amdgpu" ];
   };
