c6a58822df29ce2ff1a0fc67ab74f96fe426d508
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2026-02-02 19:52:04 +0100

    feat(networking): removed static IP from pc
    I don't need static IP when I don't host any services

--- hosts/pc/networking.nix
+++ hosts/pc/networking.nix
@@ -2,13 +2,6 @@
 with lib;
 {
   networking = {
-    interfaces.eno1 = {
-      useDHCP = false;
-      ipv4.addresses = [{
-        address = "192.168.1.167";
-        prefixLength = 24;
-      }];
-    };
     defaultGateway = "192.168.1.1";
     dhcpcd.enable = mkForce true;
     nameservers = [ "192.168.1.8" "1.1.1.1" ];
