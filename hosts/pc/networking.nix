9820df84a0a14a00a2cd1b9ef2414c5caba9752a
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2026-06-07 08:50:22 +0200

    feat(networking): set static IP for my PC
    I disabled DHCP cuz I wanna speed up OS booting

--- hosts/pc/networking.nix
+++ hosts/pc/networking.nix
@@ -1,9 +1,14 @@
-{ lib, ... }:
-with lib;
+_:
 {
   networking = {
     defaultGateway = "192.168.1.1";
-    dhcpcd.enable = mkForce true;
+    interfaces.eno1 = {
+      ipv4.addresses = [{
+        address = "192.168.1.169";
+        prefixLength = 24;
+      }];
+    };
+    dhcpcd.enable = false;
     nameservers = [ "192.168.1.8" "1.1.1.1" ];
   };
 }
