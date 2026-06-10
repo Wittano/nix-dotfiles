a0d0fdbef0f4e4c6d20e8a97a0548b4f715dffca
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2026-06-11 00:08:53 +0200

    feat(framework): added btrfs subvolumens

--- hosts/framework/hardware.nix
+++ hosts/framework/hardware.nix
@@ -3,6 +3,25 @@ _: {
     "/" = {
       device = "/dev/nixos/root";
       fsType = "btrfs";
+      options = [ "subvol=root" "compress=zstd" "noatime" ];
+      neededForBoot = true;
+    };
+    "/home" = {
+      device = "/dev/nixos/root";
+      fsType = "btrfs";
+      options = [ "subvol=home" "compress=zstd" "noatime" ];
+    };
+    "/nix" = {
+      device = "/dev/nixos/root";
+      fsType = "btrfs";
+      options = [ "subvol=nix" "compress=zstd" "noatime" ];
+      neededForBoot = true;
+    };
+    "/var/log" = {
+      device = "/dev/nixos/root";
+      fsType = "btrfs";
+      options = [ "subvol=logs" "compress=zstd" "noatime" ];
+      neededForBoot = true;
     };
     "/boot/efi" = {
       device = "/dev/disk/by-label/BOOT";
