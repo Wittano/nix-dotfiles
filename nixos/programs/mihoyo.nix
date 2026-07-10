66fef5afc1df44243aabe70f7997cffbb0aab1b0
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2026-07-10 19:05:05 +0200

    feat(anime-games): added zzz
    Also I added

--- nixos/programs/mihoyo.nix
+++ nixos/programs/mihoyo.nix
@@ -1,16 +1,31 @@
-{ lib, config, inputs, system, ... }:
+{
+  lib,
+  config,
+  inputs,
+  system,
+  ...
+}:
 with lib;
 with lib.my;
 let
   cfg = config.programs.mihoyo;
 
-  findGame = name: assert builtins.typeOf name == "string"; lists.any (x: x == name) cfg.games;
+  findGame =
+    name:
+    assert builtins.typeOf name == "string";
+    lists.any (x: x == name) cfg.games;
 in
 {
   options.programs.mihoyo = {
     enable = mkEnableOption "Enable Mihoyo games";
     games = mkOption {
-      type = with types; listOf (enum [ "genshin" "honkai-railway" ]);
+      type =
+        with types;
+        listOf (enum [
+          "genshin"
+          "honkai-railway"
+          "eepey"
+        ]);
       description = "Select which Mihoyo games should be install";
       default = [ ];
     };
@@ -21,14 +36,14 @@ in
   ];
 
   config = mkIf cfg.enable {
-    # Genshin Impact
-    programs.anime-game-launcher.enable = findGame "genshin";
-
     networking.mihoyo-telemetry.block = mkForce true;
-    # Honkai Railway
-    programs.honkers-railway-launcher = {
-      enable = findGame "honkai-railway";
-      package = inputs.aagl.packages.${system}.honkers-railway-launcher;
+    programs = {
+      anime-game-launcher.enable = findGame "genshin";
+      sleepy-launcher.enable = findGame "eepey";
+      honkers-railway-launcher = {
+        enable = findGame "honkai-railway";
+        package = inputs.aagl.packages.${system}.honkers-railway-launcher;
+      };
     };
   };
 }
