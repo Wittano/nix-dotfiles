86df6bed3487962d5d7b082170a9cb1aac653662
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2026-08-01 20:27:37 +0200

    feat(pc): enabled DHCP configuration by default

--- hosts/pc/networking.nix
+++ hosts/pc/networking.nix
@@ -1,14 +1,9 @@
-_:
-{
+_: {
   networking = {
-    defaultGateway = "192.168.1.1";
-    interfaces.eno1 = {
-      ipv4.addresses = [{
-        address = "192.168.1.167";
-        prefixLength = 24;
-      }];
-    };
-    dhcpcd.enable = false;
-    nameservers = [ "192.168.1.8" "1.1.1.1" ];
+    dhcpcd.enable = true;
+    nameservers = [
+      "192.168.1.8"
+      "1.1.1.1"
+    ];
   };
 }
