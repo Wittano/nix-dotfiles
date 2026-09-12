d1cef8c07334bfbf2f8f401fe3935ebcfce5c946
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2026-09-12 12:10:43 +0200

    feat(vivaldi): added support for PWA apps

--- home-manager/vivaldi.nix
+++ home-manager/vivaldi.nix
@@ -1,11 +1,41 @@
-{ lib, config, pkgs, ... }: with lib; {
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+with lib;
+let
+  pwaAppFixer = pkgs.writeShellApplication {
+    name = "vivaldi-pwa-fixer";
+    runtimeInputs = with pkgs; [
+      gnused
+      coreutils
+    ];
+    text = ''
+      for f in "$HOME/.local/share/applications"/vivaldi-*; do
+          echo "Replace vivaldi PWA bin path: $f"
+
+          sed -i "s/^Exec=\([a-z/0-9.-]*vivaldi\)/Exec=\/etc\/profiles\/per-user\/wittano\/bin\/vivaldi/" "$f"
+      done
+    '';
+  };
+in
+{
   options.programs.vivaldi.wittano = {
     enable = mkEnableOption "vivaldi";
     enableAutostart = mkEnableOption "vivaldi on autostart";
   };
 
   config = mkIf config.programs.vivaldi.wittano.enable {
-    home.packages = with pkgs; [ vivaldi vivaldi-ffmpeg-codecs ];
+    home = {
+      packages = with pkgs; [
+        vivaldi
+        vivaldi-ffmpeg-codecs
+        pwaAppFixer
+      ];
+      activation.fixVivaldiPWAApps = lib.hm.dag.entryBefore [ "writeBoundary" ] (meta.getExe pwaAppFixer);
+    };
 
     desktop.autostart.programs = mkIf config.programs.vivaldi.wittano.enableAutostart [ "vivaldi" ];
   };
